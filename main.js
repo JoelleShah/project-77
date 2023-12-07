7dcd2915f42ebe1a4d00d0f277a5b571360da0f1
JoelleShah <121088109+JoelleShah@users.noreply.github.com>
2023-12-07 16:06:57 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var firstName="Ranbir";
+console.log(firstName);
+
+var lastName="Kapoor";
+console.log(lastName);
+
+var fullName=firstName.concat(lastName);
+console.log(fullName);
+
+var in_de=4;
+in_de++;
+console.log(in_de);
+
+var de=11;
+de--;
+console.log(de);
+
+var add=5+10;
+console.log(add);
+
